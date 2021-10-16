00f7c0de68dcaf381115adc3040ba8d8bee5784d
popcube <40394657+popcube@users.noreply.github.com>
2021-10-16 16:10:53 +0900

    Revert "グラフ1とグラフ2wo"
    This reverts commit 069b19bee971fb5dc78636b1a0b5240e8bf270fa.

--- eff_calc_2.py
+++ eff_calc_2.py
@@ -1,5 +1,5 @@
 from matplotlib import pyplot as plt
-from matplotlib import ticker, cm, colors
+from matplotlib import ticker, cm
 
 ## 日本語表示用にフォント設定
 plt.rcParams["font.family"] = "sans-serif"
@@ -26,7 +26,7 @@ tot_res_list=[[0 for i in range(len(y_list))]]
 ## x軸メインループ
 for x in xlog_list[1:]:
     ## 処理進捗通知用
-    if(xlog_list.index(x)%5000 == 0): print(xlog_list.index(x))
+    if(xlog_list.index(x)%5000 == 0): print(x)
     
     ## あるxのときのライブボーナス消費量リストのゼロ埋め初期化
     lv_temp_list=[0 for j in range(11)]
@@ -74,53 +74,25 @@ for i, y_temp in enumerate(tot_res_list):
             ## その値とx軸とy軸の感覚値を表示する
             print(res_temp, f'x軸：{x_list[j]:.3f}', f'y軸：{y_list[i]:.1f}')
 
-## グラフ2表示のためのリスト作成
+## グラフ表示のためのリスト作成
 colorbar_labels = [i for i in range(11)]
 tick_labels = ["0"] + ["" for i in range(1, 10)] + ["大"]
 
-## グラフ1作成
-fig1, ax1 = plt.subplots()
-ax1.set_title("イベント時\nライブボーナス消費量オススメ1")
-ax1.plot(xlog_list, lv_res_list, label='ライブ報酬目当て')
-ax1.plot(xlog_list, ev_res_list, label='イベント報酬目当て')
-
-## x軸をログスケールにし、メモリとラベル作成
-ax1.set_xlim(0.1,20)
-ax1.set_xscale('log')
-ax1.xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:.1f}'))
-ax1.set_xticks([0.1, 0.2, 0.3, 0.5, 1, 2, 3, 5, 10, 20])
-ax1.set_xlabel('時間感覚')
-
-## y軸のメモリとラベル作成
-ax1.set_ylim(-1,11)
-ax1.set_yticks([i for i in range(11)])
-ax1.grid(axis='y')
-ax1.set_ylabel('ライブボーナス消費量')
-
-## 凡例の追加
-ax1.legend(bbox_to_anchor=(0,1), loc='upper left')
-
-## グラフ2作成
+## グラフ作成
 ## カラーマップは"tab20c"の最初の11色、内挿なし、グラフ外形正方形、最小値0.5、最大値0.5、ゼロ点左下
-fig2, ax2 = plt.subplots()
-ax2.imshow(tot_res_list, cmap=cm.get_cmap("tab20c", 11), interpolation='none', aspect='auto', vmin=-0.5, vmax=10.5, origin='lower')
-ax2.set_title("イベント時\nライブボーナス消費量オススメ2")
+plt.imshow(tot_res_list, cmap=cm.get_cmap("tab20c", 11), interpolation='none', aspect='auto', vmin=-0.5, vmax=10.5, origin='lower')
+plt.title("イベント時\nライブボーナス消費量オススメ")
 
 ## x軸のメモリとラベル作成
-ax2.set_xticks(ticks=[i*2000 for i in range(11)])
-ax2.set_xticklabels(tick_labels)
-ax2.set_xlabel("時間感覚")
-
+plt.xticks(ticks=[i*2000 for i in range(11)], labels=tick_labels)
+plt.xlabel("時間感覚")
 ## y軸のメモリとラベル作成
-ax2.set_yticks(ticks=[i*10 for i in range(11)])
-ax2.set_yticklabels(tick_labels)
-ax2.set_ylabel("イベント報酬重視率")
+plt.yticks(ticks=[i*10 for i in range(11)], labels=tick_labels)
+plt.ylabel("イベント報酬重視率")
 
 ## カラーバーの作成
-fig2mp = cm.ScalarMappable(norm=colors.Normalize(vmin=-0.5, vmax=10.5), cmap=cm.get_cmap("tab20c", 11))
-fig2.colorbar(mappable=fig2mp, ticks=colorbar_labels)
+plt.colorbar(ticks=colorbar_labels)
 
 ## 同じ階層のフォルダにグラフを保存して表示
-fig1.savefig("livebonus_rec1.png")
-fig2.savefig("livebonus_rec2.png")
+plt.savefig("livebonus_rec.png")
 plt.show()
